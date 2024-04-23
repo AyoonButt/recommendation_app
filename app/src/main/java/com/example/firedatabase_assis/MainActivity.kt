9452803a8ed3a856f5337feadec319f0af2185ae
Saul <srm9252@mavs.uta.edu>
2024-04-22 22:23:08 -0600

    Added Settings

--- app/src/main/java/com/example/firedatabase_assis/MainActivity.kt
+++ app/src/main/java/com/example/firedatabase_assis/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         var dbhelp = DB_class(applicationContext)
         var db = dbhelp.writableDatabase
         JobScheduler.scheduleJob(this)
